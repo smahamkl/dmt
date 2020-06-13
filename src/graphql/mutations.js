ca76f7db0ba144b974510abe669c32338d94acbb
smahamkl <tzones2006@gmail.com>
2020-06-13 10:16:36 -0700

    first commit

---
+++ src/graphql/mutations.js
@@ -0,0 +1,69 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createListing = /* GraphQL */ `
+  mutation CreateListing(
+    $input: CreateListingInput!
+    $condition: ModelListingConditionInput
+  ) {
+    createListing(input: $input, condition: $condition) {
+      id
+      title
+      company
+      url
+      status
+      favorite
+      notes
+      relatedDate
+      contactName
+      contactEmail
+      contactPhoneNumber
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const updateListing = /* GraphQL */ `
+  mutation UpdateListing(
+    $input: UpdateListingInput!
+    $condition: ModelListingConditionInput
+  ) {
+    updateListing(input: $input, condition: $condition) {
+      id
+      title
+      company
+      url
+      status
+      favorite
+      notes
+      relatedDate
+      contactName
+      contactEmail
+      contactPhoneNumber
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const deleteListing = /* GraphQL */ `
+  mutation DeleteListing(
+    $input: DeleteListingInput!
+    $condition: ModelListingConditionInput
+  ) {
+    deleteListing(input: $input, condition: $condition) {
+      id
+      title
+      company
+      url
+      status
+      favorite
+      notes
+      relatedDate
+      contactName
+      contactEmail
+      contactPhoneNumber
+      createdAt
+      updatedAt
+    }
+  }
+`;
